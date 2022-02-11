3464201f3db5fcd30335de6b49a7b7842213ca8c
easedeveloper <sespinozaea29@gmail.com>
2022-02-11 00:34:39 -0500

    HTTPS

--- cnn.js
+++ cnn.js
@@ -3,7 +3,7 @@ const pgPromise = require("pg-promise")
 const config = {
   // configuracion para la base de datos posgres en heroku
     user: 'pitscourier_postgres',
-    host: '157.90.181.209',
+    host: 'https://157.90.181.209',
     database: 'pitscourier_basenexdemo',
     password: '!agrc5i5!!',
     port: '5432',
